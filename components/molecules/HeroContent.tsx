f1c88c657a12ac43ffa4e5bd1b44d181eed08369
ZildjianVito <zildjianvito07@gmail.com>
2024-01-07 22:53:58 +0700

    fix jumbotron text header

--- components/molecules/HeroContent.tsx
+++ components/molecules/HeroContent.tsx
@@ -30,7 +30,7 @@ const HeroContent = () => {
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="mt-6 mx-auto text-3xl md:text-4xl lg:text-6xl font-bold text-white max-w-[600px] "
+          className="mt-6 text-3xl md:text-4xl lg:text-6xl font-bold text-white max-w-[600px] "
         >
           <span>
             Providing
