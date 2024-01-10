46e9a748cd8e5f83de1e41f2e5e3a9dceadd4058
ZildjianVito <zildjianvito07@gmail.com>
2024-01-10 18:23:23 +0700

    fix: change footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -47,10 +47,10 @@ const Footer = () => {
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
             <div className="font-bold text-[16px]">About</div>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Become Sponsor</span>
+              <span className="text-[15px] ml-[6px]">Kerjasama</span>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Learn with me</span>
+              <span className="text-[15px] ml-[6px]">Hubungi kami</span>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <span className="text-[15px] ml-[6px]">
