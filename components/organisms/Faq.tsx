e98b6b6a00864c920002614e85a84ea027a6fcdb
ZildjianVito <zildjianvito07@gmail.com>
2024-01-10 19:55:24 +0700

    fix: faq subheader

--- components/organisms/Faq.tsx
+++ components/organisms/Faq.tsx
@@ -10,11 +10,11 @@ import { faq } from "@/constants/faq";
 export default function Faq() {
   return (
     <section className="flex flex-col relative items-center justify-center min-h-screen w-full ">
-      <div className="flex flex-col py-10">
+      <div className="flex flex-col py-10 px-3">
         <h1 className="text-[40px] lg:text-[50px] font-semibold text-center bg-clip-text bg-gradient-to-r text-transparent from-indigo-300 to-violet-700 leading-[48px]">
           FAQ
         </h1>
-        <h3 className=" text-xl text-gray-400 my-4 max-w-[600px]">
+        <h3 className=" text-xl text-gray-400 my-4 max-w-[600px] text-center">
           Pertanyaan yang sering ditanyakan disini
         </h3>
       </div>
